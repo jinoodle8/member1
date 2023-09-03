afeb2e602635858487ab20e13883fbbb8d111de0
ablooh <ablooh@naver.com>
2023-09-03 19:46:24 +0900

    Initial commit

---
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -0,0 +1,78 @@
+package com.example.member.controller;
+
+import com.example.member.dto.MemberDTO;
+import com.example.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor //MemberService에 대한 멤버를 사용 가능
+public class MemberController {
+
+    // 생성자 주입
+    private final MemberService memberService;
+
+    // 회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")    // name값을 requestparam에 담아온다
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm(){
+        return "login";
+    }
+
+
+    @PostMapping("/member/login") // session : 로그인 유지
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            // login 실패
+            return "login";
+        }
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 model 사용
+        model.addAttribute("memberList",memberDTOList);
+        return "list";
+
+    }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        // login 처럼 return 값에 따라 분류 할 수 있음
+        model.addAttribute("member", memberDTO);
+        return "detail";
+    }
+
+    @GetMapping("/member/delete/{id}") // /member/{id}로 할 수 있도록 공부
+    public String deleteById(@PathVariable Long id){
+        memberService.deleteByid(id);
+
+        return "redirect:/member/"; // list 로 쓰면 껍데기만 보여짐
+    }
+}
+//MemberController.class
---
+++ src/main/java/com/example/member/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.example.member.repository;
+
+import com.example.member.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long>//어떤 Entity인지, pk 어떤 타입인지
+{
+    // 이메일로 회원 정보 조회( select * from member_table where member_email=?)
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
+}
+//MemberRepository.interface
